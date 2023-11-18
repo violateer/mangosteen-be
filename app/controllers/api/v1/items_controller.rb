e219c7880172bfb33bc0ee536d0c8d39542a855e
violateer <1828257089@qq.com>
2023-11-18 22:55:20 +0800

    实现items分页

---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::ItemsController < ApplicationController
+  def index
+    items = Item.page(params[:page]).per(params[:per])
+    render json: { resources: items, pager: {
+      page: params[:page] || 1,
+      per_page: params[:per] || 10,
+      count: Item.count,
+    } }
+  end
+
+  def create
+    item = Item.new amount: 1
+    if item.save
+      render json: { resource: item }
+    else
+      render json: { errors: item.errors }
+    end
+  end
+end

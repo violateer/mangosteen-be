2674c4f9bed8ea6fff85cbda42822a79fd71fd74
violateer <1828257089@qq.com>
2023-12-13 22:01:23 +0800

    发送邮件时只找最新的一个

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 class UserMailer < ApplicationMailer
   def welcome_email(email)
-    validation_code = ValidationCode.find_by_email(email)
+    validation_code = ValidationCode.order(created_at: :desc).find_by_email(email)
     @code = validation_code.code
     mail(to: email, subject: "山竹记账验证码")
   end

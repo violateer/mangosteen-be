633ae334ca0cb70a6824564f78ac3e5fbdace06f
violateer <1828257089@qq.com>
2023-12-13 21:57:36 +0800

    重构：让create validation code 更简洁

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@
 class UserMailer < ApplicationMailer
   def welcome_email(email)
     validation_code = ValidationCode.find_by_email(email)
-    @code = validation_code
-    mail(to: :email, subject: "山竹记账验证码")
+    @code = validation_code.code
+    mail(to: email, subject: "山竹记账验证码")
   end
 end

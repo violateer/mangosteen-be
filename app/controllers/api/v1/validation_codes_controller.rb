7daacd8b0b0c68d4bfefa8609b7e1b067da0c41c
violateer <1828257089@qq.com>
2023-12-13 22:24:46 +0800

    将错误的测试结果放到request里

--- app/controllers/api/v1/validation_codes_controller.rb
+++ app/controllers/api/v1/validation_codes_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::ValidationCodesController < ApplicationController
   def create
     # 判断60秒内不能重复发送验证码
+    # :too_many_requests 同 429
     return render status: :too_many_requests if ValidationCode.exists?(email: params[:email], kind: "sign_in", created_at: 1.minute.ago..Time.now)
 
     validation_code = ValidationCode.new email: params[:email], kind: "sign_in"
--- spec/models/user_spec.rb
+++
@@ -1,8 +0,0 @@
-require "rails_helper"
-
-RSpec.describe User, type: :model do
-  it "have email" do
-    user = User.new email: "xx@xx.com"
-    expect(user.email).to eq "xx@xx.com"
-  end
-end

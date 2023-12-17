e37c54f92dc7634019f98b1fd55ab6189f83bedd
violateer <1828257089@qq.com>
2023-12-17 16:00:26 +0800

    完成jwt过期检查

--- spec/requests/api/v1/mes_spec.rb
+++ spec/requests/api/v1/mes_spec.rb
@@ -1,7 +1,9 @@
 # encoding: utf-8
 require "rails_helper"
+require "active_support/testing/time_helpers"
 
 RSpec.describe "Me", type: :request do
+  include ActiveSupport::Testing::TimeHelpers
   describe "获取当前用户" do
     it "登录后成功获取" do
       user = User.create email: "1828257089@qq.com"
@@ -13,6 +15,26 @@ RSpec.describe "Me", type: :request do
       expect(response).to have_http_status(200)
       json = JSON.parse response.body
       expect(json["resource"]["id"]).to eq user.id
+    end
+
+    it "jwt过期" do
+      travel_to Time.now - 3.hours
+      user1 = User.create email: "1@qq.com"
+      jwt = user1.generate_jwt
+
+      travel_back
+      get "/api/v1/me", headers: { 'Authorization': "Bearer #{jwt}" }
+      expect(response).to have_http_status(401)
+    end
+
+    it "jwt没过期过期" do
+      travel_to Time.now - 1.hours
+      user1 = User.create email: "1@qq.com"
+      jwt = user1.generate_jwt
+
+      travel_back
+      get "/api/v1/me", headers: { 'Authorization': "Bearer #{jwt}" }
+      expect(response).to have_http_status(200)
     end
   end
 end
